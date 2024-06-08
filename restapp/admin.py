a2e09acb5b7f27b62d3f2406b22cf4468a5a7dd3
Ljubomir <ljupcedavitkov14@gmail.com>
2024-06-08 02:00:25 +0200

    Models created, Installed app in settings, Registered in admin, Made migrations and Created SuperUser

--- restapp/admin.py
+++ restapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Restaurant, Dish, DishRestaurant, Employee, BusinessHours
 # Register your models here.
+
+
+admin.site.register(Restaurant)
+admin.site.register(Dish)
+admin.site.register(DishRestaurant)
+admin.site.register(Employee)
